465421e1fcc22e06a3d109caae81eb865b8ffda2
VW Freeh <vwfreeh@ncsu.edu>
2020-07-23 11:56:52 -0400

    nginx fixes

--- farmation_front_end/lib/Constants.dart
+++ farmation_front_end/lib/Constants.dart
@@ -3,7 +3,7 @@ const int STATE_MONTHLY_CROP = 2;
 const int ECONOMICS_ANNUAL_INCOME = 3;
 const int ECONOMICS_ANNUAL_AG_LAND = 4;
 
-const String ENDPOINT_URL = 'http://localhost:8002/WsEcl/submit/query/thor/';
+const String ENDPOINT_URL = 'http://152.46.18.172:8000/esp/';
 
 const int COMPARISON_MODE_NONE = 0;
 const int COMPARISON_MODE_STATE = 1;
